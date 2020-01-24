40d196dd71906d5b7aaf5c25c6f3e3f76ba19c79
Steve Good <sgood@lanctr.com>
2020-01-23 17:01:56 -0800

    Messing around with layout and theme.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,68 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
-import logo from './logo.svg';
-import './App.css';
-import { useAppStore } from './AppContext';
+import React from "react";
+import clsx from "clsx";
+import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import theme from "./theme";
+import { CssBaseline, Container, Grid, Paper } from "@material-ui/core";
+import Header from "./Header";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  appBarSpacer: {
+    ...theme.mixins.toolbar
+  },
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto"
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column"
+  },
+  fixedHeight: {
+    height: 240
+  }
+}));
 
 const App: React.FC = () => {
-  const { state, dispatch } = useAppStore();
+  const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>A super fake login screen.</h1>
-        <p>Authenticated? {state.user.authenticated ? "true" : "false"}</p>
-        <Button variant="contained" color="primary" onClick={() => dispatch({
-          type: "authenticated",
-          user: {
-            username: state.user.authenticated ? "" : "heylookafakeusername",
-            authenticated: !state.user.authenticated
-          }
-        })}>
-          {state.user.authenticated ? `Logout: ${state.user.username}` : "Login"}
-        </Button>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <Header />
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="lg" className={classes.container}>
+            <Grid container spacing={3}>
+              {/* Chart */}
+              <Grid item xs={12} md={8} lg={9}>
+                <Paper className={fixedHeightPaper}>Paper</Paper>
+              </Grid>
+              {/* Recent Deposits */}
+              <Grid item xs={12} md={4} lg={3}>
+                <Paper className={fixedHeightPaper}>Paper</Paper>
+              </Grid>
+              {/* Recent Orders */}
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>Paper</Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </main>
+        <div>Footer</div>
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
